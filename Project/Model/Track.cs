6640674c6d1cb8caf44da9d77018319f3ea2cd7b
Philip De Rudder <philip_de_rudder@hotmail.com>
2020-04-30 16:01:08 +0200

    nieuwe dbconnectie
    vorige db gaf problemen dus opnieuw begonnen

--- Project/Model/Track.cs
+++ Project/Model/Track.cs
@@ -5,16 +5,15 @@ namespace Model
     public class Track
     {
         public int Id { get; set; }
-        public int track_id { get; set; }
         public string track_name { get; set; }
         public int track_time { get; set; }
         public string genre { get; set; }
 
         // 1 op veel relatie met Album
-        public Album Album { get; set; }
+        //public Album Album { get; set; }
 
         //Veel op veel relatie met artist --> track kan meerdere aritesten hebben
-        public ICollection<Artist> Artists { get; set; }
+       // public ICollection<Artist> Artists { get; set; }
 
     }
 }
